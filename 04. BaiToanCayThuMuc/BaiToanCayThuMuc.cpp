41046152624e90430db3c8398d8718e9fc8a7ae9
Terry <se.terry.2004@gmail.com>
2023-05-28 10:55:56 +0700

    Update BaiToanCayThuMuc.cpp

--- 04. BaiToanCayThuMuc/BaiToanCayThuMuc.cpp
+++ 04. BaiToanCayThuMuc/BaiToanCayThuMuc.cpp
@@ -15,7 +15,7 @@ int main()
 	int mem = dia.TinhDungLuong();
 	cout << "\nDung luong cua o dia la: " << mem << "." << endl;
 	int demtt = dia.DemTapTin();
-	cout << "\nSo luong tap tin trong o dia la:	" << demtt << "." << endl;
+	cout << "\nSo luong tap tin trong o dia la:	" << demtt << ".";
 	int demtmc = dia.DemThuMucCon();
 	cout << "\nSo luong thu muc con trong o dia la:	" << demtmc << "." << endl;
 	return 0;
